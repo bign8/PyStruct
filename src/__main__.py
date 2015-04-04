71aa2b984f1cd94cbe1fe668ee2f2ea5b4f10f6f
Nate Woods <Nate.Woods@workiva.com>
2015-04-04 00:14:36 -0600

    Adding timers to main method

--- src/__main__.py
+++ src/__main__.py
@@ -1,13 +1,45 @@
 from search import BNSearch
+from time import time as now
 import pprint
 
+
+class Timer(object):
+    def __init__(self, name):
+        self.name = name
+        self._start = now()
+        self._stop = self._start
+
+    def start(self):
+        self._start = now()
+        return self
+
+    def stop(self):
+        self._stop = now()
+        return self
+
+    def __repr__(self):
+        return 'Timer "{}" ran for {:.4f}s'.format(
+            self.name, self._stop - self._start
+        )
+
 if __name__ == '__main__':
+    timer = Timer('Fetching Variables')
     data = BNSearch('scale')
+    print timer.stop()
+
+    timer = Timer('Sample Probability Calculations')
     print data.probability('Class', 'L')
     print data.probability('Class', 'B')
     print data.probability('Class', 'R')
+    print timer.stop()
+
+    timer = Timer('Calculate Scores')
     data.calculate_scores()
+    print timer.stop()
+
+    timer = Timer('Search')
     data.search()
+    print timer.stop()
 
     pp = pprint.PrettyPrinter(indent=1)
     # pp.pprint(data.score.cache)
