17b559d9dc0d51cbaa5924fcefdec3b6d090a2ec
Nate Woods <Nate.Woods@workiva.com>
2015-04-08 19:58:24 -0600

    Refactor + remove clutter (again)

--- src/search.py
+++ src/search.py
@@ -1,73 +1,3 @@
-# from UUID import uuid4
-#
-#
-# class Node(object):
-#     def __init__(self):
-#         self._hash = uuid4().bytes
-#
-#     def __hash__(self):
-#         return self._hash
-#
-#
-# def neighbors(node):
-#     return []
-#
-#
-# def heuristic(node, goal):
-#     return 0
-#
-#
-# def dist(node, neighbor):
-#     return 0
-#
-#
-# def get_lowest_fscore(openset, f_score):
-#     # return the node in openset having the lowest f_score[] value
-#     keys = map(hash, openset)
-#     subset = {key: f_score[key] for key in keys}
-#     return min(subset, key=subset.get)
-#
-#
-# def reconstruct(came_from, current):
-#     total_path = [current]
-#     while current in came_from:
-#         current = came_from[hash(current)]
-#         total_path.append(current)
-#     return total_path
-#
-#
-# def search(start, goal, epsilon=1):
-#     closedset = set()  # The set of nodes already evaluated.
-#     openset = set([start])  # The set of tentative nodes to be evaluated, initially containing the start node
-#     came_from = {}  # The map of navigated nodes.
-#
-#     start_hash = hash(start)
-#     g_score = {start_hash: 0}  # Cost from start along best known path.
-#     # Estimated total cost from start to goal through y.
-#     f_score = {}
-#     f_score[start_hash] = g_score[start_hash] + epsilon * heuristic(start, goal)
-#
-#     while openset:
-#         current = get_lowest_fscore(openset, f_score)
-#         # current =
-#         if current == goal:
-#             return reconstruct(came_from, goal)
-#
-#         openset.remove(current)
-#         closedset.add(current)
-#         for neighbor in neighbors(current):
-#             if neighbor in closedset:
-#                 continue
-#             tentative_g_score = g_score[hash(current)] + dist(current, neighbor)
-#
-#             n_hash = hash(neighbor)
-#             if neighbor not in openset or tentative_g_score < g_score[n_hash]:
-#                 came_from[n_hash] = current
-#                 g_score[n_hash] = tentative_g_score
-#                 f_score[n_hash] = tentative_g_score + epsilon * heuristic(neighbor, goal)
-#                 if neighbor not in openset:
-#                     openset.add(neighbor)
-#     raise Exception('Path not found')
 from data import DataSet
 from itertools import chain, combinations
 from models import EntityCache
@@ -80,13 +10,15 @@ def powerset_generator(i):
 
 
 class BNSearch(DataSet):
-
+    """
+    TODO:
+    - Delete data structures once they are no longer necessary
+    """
     score = EntityCache()  # overridden in __main__
     best_score = EntityCache()
     base_score = {}
     parents = {}
     children = {}
-    came_from = {}
 
     def search(self):
         """
@@ -127,21 +59,16 @@ class BNSearch(DataSet):
                 if f < self.base_score.get(union, f + 1):
                     open.put((f, union))
                     self.base_score[union] = f
-                    self.came_from[union] = U
                     self.parents[union] = parents
                     self.children[union] = X
 
     def build_graph(self):
-        goal = frozenset(self.variables)
-        parents, leaves = {}, {}
-
+        goal, graph = frozenset(self.variables), {}
         while goal in self.children:
-            parents[goal] = self.parents[goal]
             leaf = self.children[goal]
-            leaves[goal] = leaf
+            graph[leaf] = self.parents[goal]
             goal = goal.difference({leaf})
-
-        return {leaves[key]: parents[key] for key in parents}
+        return graph
 
     def joint_best_score(self, Y, U):
         diff = U.difference({Y})
